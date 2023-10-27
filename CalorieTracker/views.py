d307f94169bbadc115d10fc7b2b172a314d6dc01
Giiid <120643201+giiiiid@users.noreply.github.com>
2023-10-27 15:02:37 +0000

    Getting data from an api now

--- CalorieTracker/views.py
+++ CalorieTracker/views.py
@@ -3,20 +3,16 @@ from .models import Food, Consume
 # Create your views here.
 def home(request):
     if request.method == "POST":
-
         name_of_food = request.POST['food_consumed']
-        consume = Food.objects.get(food_name=name_of_food)
-        cn = Consume(food_consumed=consume)
-        cn.save()
-        return redirect('home')
-        foods = Food.objects.all()
-        cons = Consume.objects.filter(food_consumed=consume)
 
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(name_of_food)
+        response = requests.get(api_url, headers={'X-Api-Key': 'qJXdAxQG9UWoM2g1bHQ/Cw==ODvpMi16dBa2hv7I'})
+    
+        json_data = response.json()
+        context = {'json_data':json_data}
     else:
-        foods = Food.objects.all()
-        cons = Consume.objects.all()
-
-    return render(request, 'index.html', {'foods':foods, 'cons':cons})
+        context = ''
+    return render(request, 'index.html', context)
 
 
 
